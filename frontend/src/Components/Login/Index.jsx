41b7157d59f382a632accb1bdd1bc173629f5c29
3bd-elra7man <abdelrahman_moh@yahoo.com>
2025-04-08 17:55:00 +0200

    fixed login page mobile view

--- frontend/src/Components/Login/Index.jsx
+++ frontend/src/Components/Login/Index.jsx
@@ -1,47 +1,75 @@
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import './login.css'
+import './login.css';
 import { GovernomentContext } from '../Context/GovernorateContext';
-export default function Login() {
-  let {errormsg, user, setUser, logIn, loading} = useContext(GovernomentContext);
 
-    JSON.stringify(user);
-    function getData(e){
-      setUser({
-        ...user,
-        [e.target.name]: e.target.value,
-      });
-    }
-    return (
-      <div className='log vh-100'>
-        <h1 className="text-center fw-bold py-5 headLine headLine">Login</h1>
-        <div className="loginForm p-5 wrapper rounded-4 mx-auto">
-          {errormsg?.length? <h3 className='alert alert-danger'>{errormsg}</h3>:""}
+export default function Login() {
+  let { errormsg, user, setUser, logIn, loading } = useContext(GovernomentContext);
+
+  function getData(e) {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  return (
+    <div className="log vh-100">
+      <h1 className="py-5 text-center fw-bold headLine">Login</h1>
+      <div className="container">
+        <div className="p-4 mx-auto loginForm p-md-5 rounded-4">
+          {errormsg?.length ? <h3 className="alert alert-danger">{errormsg}</h3> : ""}
           <form onSubmit={logIn}>
-            <label className='form-label  headLine fw-bold' htmlFor="email"><i class="fa-solid fa-envelope me-2"></i>E-mail</label>
-            <input onChange={getData} className='form-control mb-3' type="email" id='email' name='email' placeholder="E-mail" />
-            <label className='form-label  headLine fw-bold' htmlFor="password"><i class="fa-solid fa-lock me-2"></i>Password</label>
-            <input onChange={getData} className='form-control mb-3' type="password" id='password' name='password' placeholder="Password" />
-            <div className="d-flex justify-content-center">
-            <button type='submit' className="sub-Btn fw-bold btn bg-white rounded-5 w-50 p-3 mt-4">
-            {loading ? (
-            <div className="spinner-border text-dark" role="status">
-              <span className="sr-only">Loading...</span>
+            <div className="mb-3">
+              <label className="form-label headLine fw-bold" htmlFor="email">
+                <i className="fa-solid fa-envelope me-2"></i>E-mail
+              </label>
+              <input
+                onChange={getData}
+                className="form-control"
+                type="email"
+                id="email"
+                name="email"
+                placeholder="E-mail"
+              />
             </div>
-          ) : (
-            'Login'
-          )}
-            </button>
+            <div className="mb-3">
+              <label className="form-label headLine fw-bold" htmlFor="password">
+                <i className="fa-solid fa-lock me-2"></i>Password
+              </label>
+              <input
+                onChange={getData}
+                className="form-control"
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+              />
+            </div>
+            <div className="d-flex justify-content-center">
+              <button
+                type="submit"
+                className="p-3 mt-4 bg-white sub-Btn fw-bold btn rounded-5 w-50 w-md-25"
+              >
+                {loading ? (
+                  <div className="spinner-border text-dark" role="status">
+                    <span className="sr-only">Loading...</span>
+                  </div>
+                ) : (
+                  'Login'
+                )}
+              </button>
             </div>
           </form>
-          <div className="card-footer mt-4 text-center">
-                    <p className="text-white">
-                        <Link to={"/forgotpassword"} className="fw-bold text-white">
-                            Forgot Password?
-                        </Link>
-                    </p>
+          <div className="mt-4 text-center card-footer">
+            <p className="text-white">
+              <Link to={"/forgotpassword"} className="text-white fw-bold">
+                Forgot Password?
+              </Link>
+            </p>
+          </div>
         </div>
-        </div>
-        </div>
-    )
-  }
+      </div>
+    </div>
+  );
+}
